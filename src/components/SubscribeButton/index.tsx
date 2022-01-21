47e5e76e2f827bbe015fd7398d05090915359a20
Mesquini <mesquini@live.com>
2022-01-21 10:08:13 -0300

    add subscribe in stripe, and save data in faundadb, config webhooks stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface ISubscribeButtonProps {
@@ -5,10 +9,33 @@ interface ISubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: ISubscribeButtonProps) {
+  const { data } = useSession()
+
+  async function handleSubscribe() {
+    if(!data) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+
+  }
+
   return (
     <button 
       type="button" 
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
